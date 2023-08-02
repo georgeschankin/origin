8ebd6728a4dcb8e51c7eb09bacea32ad4cbd8802
George <goshanhiks3@mail.ru>
2023-08-02 22:04:39 +0300

    добавил задание 1 и поправил задание 2

---
+++ Lesson3/Zadacha2/counter.cpp
@@ -0,0 +1,89 @@
+﻿
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+class Counter
+{
+private:
+    int a;
+    std::string command;
+
+public:
+    Counter(int a, std::string command)
+    {
+        this->a = a;
+        this->command = command;
+    }
+
+int work()
+    {
+        setlocale(LC_ALL, "Russian");
+        SetConsoleOutputCP(1251);
+        SetConsoleCP(1251);       
+        if (command == "+")
+        {
+            a++;            
+        }
+        else if (command == "-")
+        {
+            a--;            
+        }    
+        else if (command == "=")
+        {
+            return a;
+        }
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    std::string yes_no;
+    std::string command_;
+    int a_;
+
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+    std::cin >> yes_no;
+
+    std::cout << "\n";
+
+    if (yes_no == "да")
+    {
+        std::cout << "Введите начальное значение счетчика ";
+        std::cin >> a_;
+    }
+    else if (yes_no == "нет")
+    {
+        a_ = 1;
+    }
+    
+    Counter counter();
+    do
+    {
+        std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+        std::cin >> command_;
+        Counter counter(a_, command_);
+        if ((command_ == "+") || (command_ == "-"))
+        {
+            a_ = counter.work();
+            continue;
+        }
+        else if (command_ == "=")
+        {
+            std::cout << counter.work() << std::endl;
+            continue;
+        }            
+        else if (command_ == "x")
+        {
+            std::cout << "До свидания" << std::endl;
+            break;
+        }
+                                     
+    } while (true);
+    
+}
+
+
