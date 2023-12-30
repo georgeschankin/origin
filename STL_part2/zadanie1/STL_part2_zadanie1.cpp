178eefa73791c031431e7164665965b2ceaa7931
George <goshanhiks3@mail.ru>
2023-12-30 12:05:31 +0300

    попытка 1

---
+++ STL_part2/zadanie1/STL_part2_zadanie1.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template<typename T>
+void uniq_vector(std::vector<T>& v)
+{
+    std::sort(v.begin(), v.end());
+    v.erase(std::unique(v.begin(), v.end()), v.end());
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        std::cout << v[i] << " ";
+    }
+}
+
+int main()
+{
+    std::vector <int> A = { 1, 2, 7, 3, 9, 4, 3, 2 };
+    uniq_vector(A);   
+    return 0;
+}
---
+++ STL_part2/zadanie2/STL_part2_zadanie2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <set>
+
+template<class T>
+void print_container(T& v)
+{
+    auto i = v.begin();
+    while (i != v.end())
+    {
+        std::cout << *i << " ";
+        i++;
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set);
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list);
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector);
+
+    return 0;
+}
