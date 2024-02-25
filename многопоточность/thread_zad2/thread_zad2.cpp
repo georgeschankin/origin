fb7aba160f0522abb9216493b620c89235139bd8
George <goshanhiks3@mail.ru>
2024-02-25 15:34:21 +0300

    попытка 3

--- многопоточность/thread_zad2/thread_zad2.cpp
+++ многопоточность/thread_zad2/thread_zad2.cpp
@@ -3,29 +3,34 @@
 #include <vector>
 #include <thread>
 #include <chrono>
+#include <ctime>
 
-
+const int thread_val = 2, elem_val = 1000; //кол-во потоков, кол-во элементов в векторах
+std::vector<int> vec1(elem_val), vec2(elem_val);
 
 int main()
-{
-    std::vector<int> vec1 (1000), vec2(1000);
+{  
+    setlocale(LC_ALL, "ru");   
     
+    auto start_time = clock();
     std::cout << "Количество аппаратных ядер = " << std::thread::hardware_concurrency() << std::endl;
+    for (int i = 0; i < thread_val; i++)
+    {
+        for (int j = 0; j < elem_val; j++)
+        {                      
+            std::vector<int> vector_thread(thread_val);
+            
+            std::thread vector_thread[j](vec1.size()); //возникает ошибка. как элементам вектора присвоить значение потока?
 
-    std::thread th1(vec1.size());
-    auto start_thread1 = std::chrono::steady_clock::now();
-    auto end_thread1 = std::chrono::steady_clock::now();
-    std::chrono::duration<double> time_thread1 = end_thread1 - start_thread1;
-    std::cout << "1 потоков: " << time_thread1.count() << "\t";
+            std::thread th1(vec1.size());
+            std::thread th2(vec1.size());
 
-    std::thread th2(vec2.size());
-    auto start_thread2 = std::chrono::steady_clock::now();
-    auto end_thread2 = std::chrono::steady_clock::now();
-    std::chrono::duration<double> time_thread2 = end_thread2 - start_thread2;
-    std::cout << "1 потоков: " << time_thread2.count() << "\t";
-   
-    th1.join();
-    th2.join();
+            th1.join();
+            th2.join();                     
+        }
+    }
+    auto end_time = clock();
+    std::cout << "2 потока: " << end_time - start_time << " тиков или " << ((float)(end_time - start_time)) / CLOCKS_PER_SEC << " секунд(ы)" << std::endl;   
 
     return 0;
 }
