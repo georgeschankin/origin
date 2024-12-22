d5e56d53b2de186f35afc7e1d05f52d5af11f569
George <goshanhiks3@mail.ru>
2024-12-22 22:05:32 +0300

    попытка 2

---
+++ асинхронность/задание_2/AsyncZad2.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <future>
+
+int block = 4;
+
+void func(int& a)
+{
+    a++;
+}
+
+template <typename Iterator>
+void parallelForEachAsyncRecursive(Iterator begin, Iterator end, /*void(*f)(int)*/ std::function<int(int*)> f)
+{   
+    auto size = std::distance(begin, end);
+
+    if (size <= block)
+    {
+        std::for_each(begin, end, f);
+    }
+    else
+    {
+        auto mid = begin;      
+        mid += size / 2;
+        auto ftRes = std::async(parallelForEachAsyncRecursive<Iterator>, begin, mid, f);
+        parallelForEachAsyncRecursive(mid, end, f);
+
+        ftRes.get();
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    std::vector<int> vec = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    std::cout << "Первоначальный: ";
+    for (int i : vec)
+    {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    parallelForEachAsyncRecursive(vec.begin(), vec.end(), func);
+
+    std::cout << "\nПосле: ";
+    for (int i : vec)
+    {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
