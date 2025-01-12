4bd2f0fff352722f551f67480e0449c6c014172c
George <goshanhiks3@mail.ru>
2025-01-12 15:46:50 +0300

    попытка 2

---
+++ Курсовая_потокобезопасная_очередь/Thread_Pool.cpp
@@ -0,0 +1,16 @@
+#include "Thread_Pool.h"
+
+void Thread_Pool::work()
+{	
+	std::unique_lock<std::mutex> lock(m);
+	s_queue.pop();
+}
+
+void Thread_Pool::submit(std::vector<std::function<void()>> fun)
+{	
+	for (int j = 0; j < fun.size(); ++j)
+	{
+		s_queue.push(fun.at(j));
+	}		
+		
+}
---
+++ Курсовая_потокобезопасная_очередь/Thread_Pool.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Safe_Queue.h"
+
+#include <thread>
+#include <vector>
+#include <mutex>
+#include <functional>
+
+class Thread_Pool
+{
+	private:
+		std::vector<std::thread> vec_th;
+		std::mutex m; //дл€ реализации блокировки
+		Safe_Queue<std::function<void()>> s_queue; //дл€ использовани€ функций из класса Safe_Queue		
+
+	public:
+		Thread_Pool(int cores)
+		{			
+			for (int i = 0; i < cores; ++i)
+			{
+				vec_th.push_back(std::thread(&Thread_Pool::work, this));
+			}
+		}		
+
+		//метод work Ч выбирает из очереди очередную задачу и исполн€ет еЄ. Ётот метод передаЄтс€ конструктору потоков дл€ исполнени€;
+		void work();
+
+		//метод submit Ч помещает в очередь задачу.
+		//¬ качестве аргумента метод может принимать или объект шаблона std::function, или объект шаблона package_task;
+		void submit(std::vector<std::function<void()>> fun);
+};
