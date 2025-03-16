1d723dfdbaab7b2e8cd33ba1c559b13b2b238ca4
George <goshanhiks3@mail.ru>
2025-03-16 13:44:49 +0300

    попытка 1

---
+++ Qt/zadanie9/udpworker.h
@@ -0,0 +1,30 @@
+#ifndef UDPWORKER_H
+#define UDPWORKER_H
+
+#include <QUdpSocket>
+#include <QNetworkDatagram>
+#include <QDateTime>
+
+#define BIND_PORT 12345
+
+class UDPworker : public QObject
+{
+    Q_OBJECT
+public:
+    explicit UDPworker(QObject *parent = nullptr);
+    void InitSocket(void);
+    void ReadDatagram(QNetworkDatagram datagram);
+    void SendDatagram(QByteArray data);
+
+private slots:
+    void readPendingDatagrams();
+
+private:
+    QUdpSocket* serviceUdpSocket;
+
+signals:
+    void sig_sendTimeToGUI(QDateTime data);
+
+};
+
+#endif // UDPWORKER_H
